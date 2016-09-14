ac9fb43b18141392d017475dce371e86e849235a
R. Kevin Nelson <kevin@rkn.la>
2016-09-14 17:19:35 +0200

    Initial Commit

---
+++ Swift3DMetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3DMetal
+//
+//  Created by Kevin Nelson on 14/09/16.
+//  Copyright © 2016 rknLA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
